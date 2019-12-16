cf1a0e54fb3b9d376acddae750a34dea7b2029c0
Roy Davis <myroy1024@yahoo.com>
2019-12-16 17:19:29 -0500

    functionality almost done

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [
+    {
+        name: "Brendan",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-ePg7QNCySz1VF-NdCEbm3xXYwU8ppgOQDKoua1g5u2ou8o_x&s",
+        scores: [5,2,3,4,3,2,1,5,4,1]
+    },
+    {
+        name: "Maria",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn5eBcUX9E6ftVzot9sQaf45ui8gMVPUVj5sG5E4HwcNMv7woP&s",
+        scores: [3,1,3,5,4,3,2,4,5,1]
+    },
+    {
+        name: "Daniel",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYLIIuUtrvHLLheJ4vrV4hNQ0kh10wVyAmVcGU1giWtQ9pSeiH&s",
+        scores: [5,1,3,3,3,4,2,2,5,5]
+    }
+];
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,52 @@
-const friendsData = require("../data/friends");
+const friends = require("../data/friends.js");
 
-module.exports = function(app){
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res){
-        res.json(friendsData);
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+
+    app.post("/api/friends", function (req, res) {
+
+        var totalDifference = 0;
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+        let user = req.body;
+        let userName = user.name;
+        let userScores = user.scores;
+        
+
+        let num = userScores.map(function(item){
+            return parseInt(item, 10);
+        });
+        user = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: num
+        };
+
+        let sum = num.reduce((a, b) => a + b, 0);
+
+        for(let i = 0; i < friends.length; i++){
+            totalDifference = 0;
+
+            let friendScore = friends[i].scores.reduce((a,b) => a + b, 0);
+            totalDifference += Math.abs(sum - friendScore);
+            if(totalDifference <= bestMatch.friendDifference){
+                bestMatch.name = friends[i].name;
+                bestMatch.photo = friends[i].photo;
+                bestMatch.friendDifference = totalDifference;
+            }
+        }
+        
+            friends.push(user);
+            console.log("Friend added");
+            res.json(bestMatch);
+    
+
     });
 };
-
-app.post("/api/friends", function(req,res){
-
-    
-})
