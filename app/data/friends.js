2283964e23523214817d559cd64b1ec37a7b02e4
Roy Davis <myroy1024@yahoo.com>
2019-12-19 15:04:44 -0500

    added some friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,6 +13,21 @@ var friends = [
         name: "Daniel",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYLIIuUtrvHLLheJ4vrV4hNQ0kh10wVyAmVcGU1giWtQ9pSeiH&s",
         scores: [5,1,3,3,3,4,2,2,5,5]
+    },
+    {
+        name: "Stephanie",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRy8e9ET1kBT3fMOS_lS8OG3fO1qvykWHz4SmTGz6vMO0rlvuKPw&s",
+        scores: [2,2,3,4,3,4,2,2,3,5]
+    },
+    {
+        name: "Rachel",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8nMI0WjIJLt1AZDjix_Dv_UtarHdbLYBNpAkdVUqEMwaPvTvTvg&s",
+        scores: [4,1,3,3,3,2,2,2,1,3]
+    },
+    {
+        name: "Mark",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgCNUWOsjFXHFaLY5egO6owiRLkGGDRFj5iQlIjF_pWSUWnSRcoQ&s",
+        scores: [3,1,3,2,3,4,2,2,3,4]
     }
 ];
 
